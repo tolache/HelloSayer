4262cc4e941c354f92e8a1159bc40ed5f7c54b40
tolache <anatoly.cherenkov@gmail.com>
2019-02-08 13:05:35 +0300

    Disable auto-generated AssemblyInfo and manually add it to the project

---
+++ HelloSayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("HelloSayer")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.18.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.18")]
+[assembly: System.Reflection.AssemblyProductAttribute("HelloSayer")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HelloSayer")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.18.0")]
