c28a72d99042425584c65ff14081c1350ba32e6e
tolache <anatoly.cherenkov@gmail.com>
2019-09-16 18:53:31 +0300

    Update version to 26

--- HelloSayer/HelloSayer.cs
+++ HelloSayer/HelloSayer.cs
@@ -6,7 +6,7 @@ namespace HelloSayer
     {
         public static void SayHello(string name, int linesCount)
         {
-            Console.WriteLine("HelloSayer version 25");
+            Console.WriteLine("HelloSayer version 26");
             for (int i = 1; i <= linesCount; i++)
             {
                 Console.WriteLine("Hello {0}! This is line #{1}.", name, i);
