b7470ed9c900a0e83d54b840e4177b09c9f61e30
tolache <anatoly.cherenkov@gmail.com>
2020-10-15 15:59:29 +0300

    update version and SDK

--- HelloSayer/HelloSayer.cs
+++ HelloSayer/HelloSayer.cs
@@ -7,7 +7,7 @@ namespace HelloSayer
     {
         public static void SayHello(string name, int linesCount)
         {
-            Console.WriteLine("HelloSayer version 26");
+            Console.WriteLine("HelloSayer version 27");
             for (int i = 1; i <= linesCount; i++)
             {
                 Console.WriteLine("Hello {0}! This is line #{1}.", name, i);
