00a8b47d536d6d76790d9860e2323e0b6a0199e9
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2021-03-03 15:51:26 +0300

    try fixing the build

--- HelloSayer/HelloSayer.cs
+++ HelloSayer/HelloSayer.cs
@@ -7,7 +7,7 @@ namespace HelloSayer
     {
         public static void SayHello(string name, int linesCount)
         {
-            Console.WriteLine("HelloSayer version 28");
+            Console.WriteLine("HelloSayer version 29");
             for (int i = 1; i <= linesCount; i++)
             {
                 Console.WriteLine("Hello {0}! This is line #{1}.", name, i);
