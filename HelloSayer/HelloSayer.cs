702be2893f217c113ce72c220072892fc80c0214
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2021-03-03 15:05:57 +0300

    Break the build

--- HelloSayer/HelloSayer.cs
+++ HelloSayer/HelloSayer.cs
@@ -13,17 +13,5 @@ namespace HelloSayer
                 Console.WriteLine("Hello {0}! This is line #{1}.", name, i);
             }
         }
-
-        public static string useNewtonsoftJson()
-        {
-            Account account = new Account
-            {
-                Name = "John Doe",
-                Email = "john@microsoft.com",
-                DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc),
-            };
-            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
-            return json;
-        }
     }
 }
