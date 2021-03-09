16053351fe4b54000792d563524b40013e73f0db
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2021-03-09 19:02:37 +0300

    fix build

--- HelloSayer/HelloSayer.cs
+++ HelloSayer/HelloSayer.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using NewtonSoft.Json;
 
 namespace HelloSayer
 {
@@ -7,7 +6,7 @@ namespace HelloSayer
     {
         public static void SayHello(string name, int linesCount)
         {
-            Console.WriteLine("HelloSayer version 29");
+            Console.WriteLine("HelloSayer version 31");
             for (int i = 1; i <= linesCount; i++)
             {
                 Console.WriteLine("Hello {0}! This is line #{1}.", name, i);
